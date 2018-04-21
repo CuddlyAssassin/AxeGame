4c7d8fa998e1c8d30f31158f4e01540852dde0b4
xxMadMan <kalebismadman@gmail.com>
2018-04-21 15:39:44 +0930

    Made cannon fire

---
+++ Assets/Code/Game/CannonSpawn.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CannonSpawn : MonoBehaviour {
+
+    [SerializeField]
+    private float cannonShootSpeed;
+    [SerializeField]
+    private float cannonReset;
+    [SerializeField]
+    private float resetTimer;
+
+    [SerializeField]
+    private GameObject cannonBall;
+    [SerializeField]
+    private GameObject spawnPoint;
+
+    [SerializeField]
+    private bool _shot = false;
+
+    private AudioSource fired;
+
+    // Use this for initialization
+    void Start () {
+        resetTimer = cannonReset;
+        fired = gameObject.GetComponent<AudioSource>();
+	}
+	
+    void Update()
+    {
+        if (_shot == true)
+        {
+            resetTimer -= Time.deltaTime;
+            if (resetTimer <= 0)
+            {
+                _shot = false;
+                resetTimer = cannonReset;
+            }
+        }
+    }
+
+    void OnTriggerStay()
+    {
+        if (_shot == false)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Fire();
+            }
+        }
+    }
+
+    void Fire()
+    {
+        _shot = true;
+        fired.Play();
+        GameObject CannonClone = Instantiate(cannonBall, spawnPoint.transform.position, spawnPoint.transform.rotation);
+        Rigidbody force = CannonClone.GetComponent<Rigidbody>();
+        force.velocity = spawnPoint.transform.forward * cannonShootSpeed;
+        NetworkServer.Spawn(CannonClone);
+    }
+}
