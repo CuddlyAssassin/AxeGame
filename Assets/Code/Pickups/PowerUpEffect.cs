8805ebc189ab8c67192202a59b86807305b028b1
Unknown <kalebismadman@gmail.com>
2019-01-17 01:42:26 +1100

    Pickups fixed
    Now the rotate and bob

--- Assets/Code/Pickups/PowerUpEffect.cs
+++ Assets/Code/Pickups/PowerUpEffect.cs
@@ -8,46 +8,10 @@ public class PowerUpEffect : MonoBehaviour
     public float rotSpeed;
 
     float bounceSpeed = 0.006f;
-
-    bool _up = true;
-    bool _down;
-
     // Update is called once per frame
     void Update()
     {
 
         transform.Rotate(0, rotSpeed * Time.deltaTime, 0);
-
-        Bounce();
-
-        if (Input.GetKeyDown(KeyCode.M))
-        {
-            print(transform.position.y);
-        }
-
-        if (0.5 >= transform.position.y)
-        {
-            _up = false;
-            _down = true;
-        }
-
-        if (-0.5 >= transform.position.y)
-        {
-            _down = false;
-            _up = true;
-        }
-    }
-
-    void Bounce()
-    {
-        if (_up == true)
-        {
-            transform.position += transform.up * Time.deltaTime;
-        }
-
-        if (_down == true)
-        {
-            transform.position -= transform.up * Time.deltaTime;
-        }
     }
 }
